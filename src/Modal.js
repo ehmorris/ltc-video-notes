73310f203e5cebd7e506f74f9f5ff5b22d0d0f69
Edwin Morris <edwin@rapgenius.com>
2018-09-22 18:20:11 -0400

    Animate modals in

--- src/Modal.js
+++ src/Modal.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import styled from 'react-emotion';
+import { Spring } from 'react-spring';
 
-const Modal = ({onClick, children, type = 'default'}) => {
+const Modal = ({...styles, children}) => {
   return (
-    <ModalContainer>
-      {children}
-    </ModalContainer>
+    <ModalContainer
+      {...styles}
+    >{children}</ModalContainer>
   );
 }
 
-export default Modal;
+const AnimatedModal = ({children}) => {
+  return (
+    <Spring
+      from={{ opacity: .5, scale: .98 }}
+      to={{ opacity: 1, scale: 1 }}
+      config={{ duration: 200 }}
+    >
+      {styles => <Modal {...styles} children={children} /> }
+    </Spring>
+  );
+}
+
+export default AnimatedModal;
 
 const ModalContainer = styled('div')`
+  opacity: ${props => props.opacity};
   position: absolute;
   width: 500px;
   height: 500px;
@@ -22,6 +36,7 @@ const ModalContainer = styled('div')`
   padding: 48px;
   left: 50%;
   top: 50%;
-  transform: translate3d(-50%, -50%, 0);
+  transform: scale(${props => props.scale}) translate3d(-50%, -50%, 0);
+  transform-origin: bottom left;
   justify-content: space-between;
 `;

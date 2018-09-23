8e2675e1c6e523a37f39786a1408abcd782c1343
Edwin Morris <edwin@rapgenius.com>
2018-09-23 15:36:40 -0400

    Fun hover effect

--- src/Button.js
+++ src/Button.js
@@ -1,37 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'react-emotion';
 
-const Button = ({onClick, children, disabled = false, type = 'default'}) => {
-  return (
-    <div>
-      {disabled &&
-        <DisabledButtonContainer>
-          {children}
-        </DisabledButtonContainer>
-      }
-
-      {!disabled &&
-        <ButtonContainer onClick={onClick}>
-          {children}
-        </ButtonContainer>
-      }
-    </div>
-  );
+const clamp = (number, min, max) => {
+  return Math.max(min, Math.min(max, number));
 }
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+
+    this.nudgeButton = this.nudgeButton.bind(this);
+    this.resetButton = this.resetButton.bind(this);
+    this.followMouse = this.followMouse.bind(this);
+
+    this.state = {
+      buttonX: 0,
+      buttonY: 0,
+    }
+  }
+
+  nudgeButton({movementX, movementY}) {
+    this.setState({
+      buttonX: clamp(movementX / 2, -5, 5),
+      buttonY: clamp(movementY / 2, -5, 5),
+    });
+  }
+
+  followMouse({movementX, movementY}) {
+    this.setState({
+      buttonX: clamp(this.state.buttonX + movementX, -5, 5),
+      buttonY: clamp(this.state.buttonY + movementY, -5, 5),
+    });
+  }
+
+  resetButton() {
+    console.log(`0, 0`);
+    this.setState({
+      buttonX: 0,
+      buttonY: 0,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.disabled &&
+          <DisabledButtonContainer>
+            {this.props.children}
+          </DisabledButtonContainer>
+        }
+
+        {!this.props.disabled &&
+          <div
+            onClick={this.props.onClick}
+            onMouseOver={this.nudgeButton}
+            onMouseMove={this.followMouse}
+            onMouseOut={this.resetButton}
+            onMouseDown={this.resetButton}
+            style={{
+              ...buttonContainer,
+              transform: `translate(${this.state.buttonX}px, ${this.state.buttonY}px)`,
+              textShadow: `
+                ${-this.state.buttonX}px
+                ${-this.state.buttonY}px
+                ${(Math.abs(this.state.buttonX) + Math.abs(this.state.buttonY)) / 2}px
+                rgba(0, 0, 0, .2)`,
+            }}
+          >
+            {this.props.children}
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+};
 
 export default Button;
 
-const ButtonContainer = styled('div')`
-  font-weight: 600;
-  cursor: pointer;
-  user-select: none;
-  text-decoration: none;
-  color: inherit;
-
-  :hover {
-    text-shadow: -3px 5px 12px rgba(0, 255, 0, .1);
-  }
-`;
+const buttonContainer = {
+  fontWeight: '600',
+  cursor: 'pointer',
+  userSelect: 'none',
+  textDecoration: 'none',
+  color: 'inherit',
+  transition: 'transform .2s ease, text-shadow .2s ease',
+  padding: '1rem',
+  margin: '-1rem',
+};
 
 const DisabledButtonContainer = styled('div')`
   font-weight: 600;

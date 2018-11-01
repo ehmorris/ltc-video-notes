de0f2b3a85c117318cad1cc6c1ff9435719ec67d
Edwin Morris <edwin@rapgenius.com>
2018-11-01 12:02:06 -0400

    Update uninitialized mode message

--- src/UninitializedMode.js
+++ src/UninitializedMode.js
@@ -7,7 +7,7 @@ const UninitializedMode = ({style}) => {
     <Container>
       <div>First click “Begin recording”, and you’ll see producer mode, then open a new window and it’ll automatically turn into interviewer mode.</div>
       <Label>
-        1h20m of runtime; No LTC will be transmitted.
+        2h00m of runtime; No LTC will be transmitted.
         <br />
         <br />
         To fullscreen the new window in Chrome, press control+command+F, then shift+command+F to hide the toolbar.
